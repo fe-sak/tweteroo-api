7e98135e5e70564e36c77170510567f1915fa348
Sakuma <felipe.sakuma@capco.com>
2023-01-17 15:55:20 -0300

    add get /tweets route

--- src/main/java/com/tweteroo/api/controller/Tweet.java
+++ src/main/java/com/tweteroo/api/controller/Tweet.java
@@ -1,9 +1,11 @@
 package com.tweteroo.api.controller;
 
 import com.tweteroo.api.dto.TweetRequestBodyDTO;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +15,12 @@ public class Tweet {
   @PostMapping
   public String create(@RequestBody TweetRequestBodyDTO requestBody) {
     // TODO Add data persistence for tweet
+    return "OK";
+  }
+
+  @GetMapping
+  public String read(@RequestParam String page){
+    // TODO Return tweets by page
     return "OK";
   }
 }
