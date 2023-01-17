24b5e769380b4700757ee7541b396d5b5346f1c0
Sakuma <felipe.sakuma@capco.com>
2023-01-17 15:34:44 -0300

    initialize project basic structure

---
+++ src/main/java/com/tweteroo/api/controller/Authorization.java
@@ -0,0 +1,17 @@
+package com.tweteroo.api.controller;
+
+import com.tweteroo.api.dto.UserDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+public class Authorization {
+
+  @PostMapping("/sign-up")
+  public void signUp(@RequestBody UserDTO req){
+    System.out.println(req);
+  }
+}
