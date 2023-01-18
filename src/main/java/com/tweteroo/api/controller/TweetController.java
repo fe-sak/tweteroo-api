eeef50427a39a71d65ffdde4af5d8c9ca251a9dd
Sakuma <felipe.sakuma@capco.com>
2023-01-18 18:47:14 -0300

    add read tweets funcionality

--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -5,6 +5,7 @@ import com.tweteroo.api.model.Tweet;
 import com.tweteroo.api.model.User;
 import com.tweteroo.api.repository.TweetRepository;
 import com.tweteroo.api.repository.UserRepository;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,14 +39,45 @@ public class TweetController {
   }
 
   @GetMapping
-  public String read(@RequestParam String page) {
-    // TODO Return tweets by page
-    return "OK";
+  public List<Tweet> read(@RequestParam String page) {
+    if (page.isBlank() || page.isEmpty()) {
+      return getLastFiveTweets();
+    } else {
+      return getTweetsByPage(page);
+    }
   }
 
   @GetMapping("/{username}")
   public List<Tweet> readByUser(@PathVariable String username) {
 
     return tweetRepository.findByUsername(username);
+  }
+
+  private List<Tweet> getLastFiveTweets() {
+    List<Tweet> lastFive = new ArrayList<>();
+    List<Tweet> tweets = tweetRepository.findAll();
+
+    int size = tweets.size();
+
+    for (int i = size - 1; i >= size - 5 && i >= 0; i--) {
+      lastFive.add(tweets.get(i));
+    }
+
+    return lastFive;
+  }
+
+  private List<Tweet> getTweetsByPage(String pageString) {
+    int page = Integer.parseInt(pageString);
+
+    if (page <= 0) return new ArrayList<>(); // TODO Modify to return http error
+
+    List<Tweet> unpaginatedTweets = tweetRepository.findAll();
+    List<Tweet> paginatedTweets = new ArrayList<>();
+
+    for (int i = (page - 1) * 5; i < page * 5 && i < unpaginatedTweets.size(); i++) {
+      paginatedTweets.add(unpaginatedTweets.get(i));
+    }
+
+    return paginatedTweets;
   }
 }
